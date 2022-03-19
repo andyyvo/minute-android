0c529b8a1c8da7cf4a25349b038013e41063dea2
cho-bani <57870635+cho-bani@users.noreply.github.com>
2022-03-19 14:57:21 -0400

    added to emptytest and updated recording_full.xml
    Signed-off-by: cho-bani <57870635+cho-bani@users.noreply.github.com>

--- app/src/main/java/com/example/minute/recording/emptytest.java
+++ app/src/main/java/com/example/minute/recording/emptytest.java
@@ -3,13 +3,32 @@ package com.example.minute.recording;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.telecom.Call;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.minute.R;
+import com.google.android.material.snackbar.Snackbar;
+
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class emptytest extends AppCompatActivity {
 
     TextView helloText;
+    private Button btnStart;
+    private TextView textSnackbar;
+
+    private Call mCall;
+    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+
+    // access code
+    private String accessTok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,26 @@ public class emptytest extends AppCompatActivity {
         setContentView(R.layout.activity_emptytest);
 
         helloText = (TextView) findViewById(R.id.helloText);
-        helloText.setText("Hello World!");
+        helloText.setText("hi! This is a sentence I think I would maybe like to test");
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dothing();
+            }
+        });
+    }
+
+    public void dothing() {
+        helloText.setText("thing done");
+
+        if (accessTok == null) {
+            final Snackbar snackbar = Snackbar.make(textSnackbar, "Lack Access Token", Snackbar.LENGTH_SHORT);
+            snackbar.show();
+            return;
+        }
+
+
     }
 }
